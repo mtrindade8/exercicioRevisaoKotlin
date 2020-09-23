774e567a9c721a1b7d4e781701b6ecc8f751fd10
Mateus <mateustrindade8@gmail.com>
2020-09-23 20:58:29 -0300

    Inclusão Colecao || Livro || Produto

--- src/com/digitalHouse/exRevisao/Produto.kt
+++ src/com/digitalHouse/exRevisao/Produto.kt
@@ -1,4 +1,11 @@
 package com.digitalHouse.exRevisao
 
 interface Produto {
+
+    abstract var codigo: Int
+    abstract var qtdEstoque: Int
+    abstract var preco: Double
+
+    override fun toString(): String
+
 }
